3c881f282b82ea74ced34294c4ffb8a4d82789f6
Joao Ferreira <joaovlima07@hotmail.com>
2022-01-23 15:14:02 -0300

    feat: express

---
+++ src/infra/http/app.ts
@@ -0,0 +1,16 @@
+import 'dotenv/config'
+import 'reflect-metadata'
+import 'express-async-errors'
+import express, { Request, Response, NextFunction } from 'express'
+
+
+const app = express()
+app.use(express.json())
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    if (err) {
+        return res.json({ message: err.message })
+    }
+    return res.status(500).json({ status: 'error', message: `internal server error - ${err.message}` })
+})
+
+export { app }
